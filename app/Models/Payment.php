41371ff3901ce8b96522aca0b894fa6408fb0903
Lois Onyinyemme Bassey <loisbassey@gmail.com>
2024-10-22 01:28:44 +0100

    payments table model class

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Payment extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['invoice_id', 'amount', 'method'];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class);
+    }
 }
